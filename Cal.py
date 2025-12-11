19a3c381cecf90b9735f8b3436070afde70d0179
sivakiran-09 <polusiva.kiran2023@vitstudent.ac.in>
2025-12-11 21:20:02 +0530

    Updated

--- Cal.py
+++ Cal.py
@@ -6,15 +6,23 @@ def add(x,y):
     return x+y
 def sub(x,y):
     return x-y
-
+def mul(x,y):
+    return x*y
+def div(x,y):
+    return x/y
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
-
-choice=input("Enter choice(1/2): ")
+print("3.Multiply")
+print("4.Divide")
+choice=input("Enter choice(1/2/3/4): ")
 if choice=='1':
     print(a,"+",b,"=",add(a,b))
 elif choice=='2':
     print(a,"-",b,"=",sub(a,b))
+elif choice=='3':
+    print(a,"*",b,"=",mul(a,b))
+elif choice=='4':
+    print(a,"/",b,"=",div(a,b))
 else:
     print("Invalid input")
